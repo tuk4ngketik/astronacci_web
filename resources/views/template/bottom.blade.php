94f843218df0109add294b01ec06de3c94874a42
tuk4ngKetik <tuk4ng.ketik@gmail.com>
2025-10-02 20:47:16 +0700

    create layout template

--- resources/views/template/bottom.blade.php
+++ resources/views/template/bottom.blade.php
@@ -1,10 +1,11 @@
+<footer> 
+    <p align="center" class="mt-5">
+        &copy; {{ date('Y') }} Astronacci
+    </p>
+</footer>
 
-    <footer>
-        <p>&copy; {{ date('Y') }} Astronacci.</p>
-    </footer>
-
-    <script src="{{ asset('js/app.js') }}"></script>
-    @yield('scripts') 
+<script src="{{ asset('js/app.js') }}"></script>
+@yield('scripts') 
 
 </body>
 </html>
--- resources/views/template/top.blade.php
+++ resources/views/template/top.blade.php
@@ -9,31 +9,41 @@
     <script src="{{ asset('plugins/jquery-3.7.1.min.js') }}"></script>
     <script src="{{ asset('plugins/bootstrap5/bootstrap.bundle.min.js') }}"></script>
 </head>
+<body><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Astronacci @yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('plugins/bootstrap5/bootstrap.min.css') }}">
+    
+    <script src="{{ asset('plugins/jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ asset('plugins/bootstrap5/bootstrap.bundle.min.js') }}"></script>
+</head>
 <body>
 
-    <header> 
-        <nav class="navbar bg-primary" data-bs-theme="dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar w/ text</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-                </li>
-            </ul>
-            <span class="navbar-text">
-                Navbar text with an inline element
-            </span>
-            </div>
+<nav class="navbar navbar-expand-lg  bg-primary bg-body-dark"> 
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="#">Astronacci</a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
         </div>
-        </nav>
-    </header>
+    </div>
+</nav> 
