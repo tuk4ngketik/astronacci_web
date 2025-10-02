fe0fd45e173b8b1113455695a86cb51102c6cecf
tuk4ngKetik <tuk4ng.ketik@gmail.com>
2025-10-02 20:51:38 +0700

    add route login n middleware

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\CekAdmin;     
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware([CekAdmin::class])->group(function () { 
+    
+    Route::get('/',[DashboardController::class, 'list_user']);
+
 });
-
 
 Route::get('/login', function () {
     return view('login');
-});
+})->name('login');
+
+Route::post('/login-act',[LoginController::class, 'login_act']);
