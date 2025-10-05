e028450a1f411b77a5a087bb081f0859c78c282e
tuk4ngKetik <tuk4ng.ketik@gmail.com>
2025-10-05 07:28:27 +0700

    Add link Logout

--- resources/views/template/top.blade.php
+++ resources/views/template/top.blade.php
@@ -40,9 +40,8 @@
             <a class="nav-link disabled" aria-disabled="true">Disabled</a>
             </li>
         </ul>
-        <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button class="btn btn-outline-success" type="submit">Search</button>
+        <form class="d-flex" role="search"> 
+            <a class="btn btn-outline-success" href="{{ url('logout') }}">Logout</a>
         </form>
         </div>
     </div>
